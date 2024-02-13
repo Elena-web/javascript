e97491a693fdacb40ac971ba063cc32bfe554706
Elena <110896958+Elena-web@users.noreply.github.com>
2024-02-13 12:06:50 +0300

    feat: add function

--- series/series.js
+++ series/series.js
@@ -1,14 +1,37 @@
-//
 // This is only a SKELETON file for the 'Series' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
 export class Series {
   constructor(series) {
-    throw new Error('Remove this statement and implement this function');
+    this._series = series;
   }
 
   slices(sliceLength) {
-    throw new Error('Remove this statement and implement this function');
+    if(this._series.length === 0) {
+      throw new Error('series cannot be empty');
+    }
+    if(this._series.length < sliceLength) {
+      throw new Error('slice length cannot be greater than series length');
+    }
+    if(sliceLength === 0) {
+      throw new Error('slice length cannot be zero');
+    }
+    if(sliceLength < 0) {
+      throw new Error('slice length cannot be negative');
+    }
+
+    let arr = [];
+
+    for(let i = 0; i <= this._series.length - sliceLength; i++) {
+      let arr1 = []; 
+      for(let j = 0; j < sliceLength; j++) {
+          arr1.push(parseInt(this._series.substring(i + j, i + j + 1)));
+      }
+      arr.push(arr1);
+  }
+
+  return arr;
   }
 }
+
