8bce1bfb66690d56725c9443840ce2508c1ce216
Elena <110896958+Elena-web@users.noreply.github.com>
2024-03-19 16:16:29 +0300

    feat: add function commands

--- secret_handshake/secret-handshake.js
+++ secret_handshake/secret-handshake.js
@@ -3,6 +3,27 @@
 // convenience to get you started writing code faster.
 //
 
-export const commands = () => {
-  throw new Error('Remove this statement and implement this function');
+export const commands = (num) => {
+  const arr = [];
+  if (num > 0 && num < 32) {
+    const numBinary = num.toString(2);
+    if (numBinary.length <= 5) {
+      if (numBinary[numBinary.length - 1] === '1') {
+        arr.push('wink');
+      }
+      if (numBinary[numBinary.length - 2] === '1') {
+        arr.push('double blink');
+      }
+      if (numBinary[numBinary.length - 3] === '1') {
+        arr.push('close your eyes');
+      }
+      if (numBinary[numBinary.length - 4] === '1') {
+        arr.push('jump');
+      }
+      if (numBinary[numBinary.length - 5] === '1') {
+        arr.reverse();
+      }
+    }
+  }
+  return arr;
 };
