7d43e6f8eb5e4f51283a4b17beb3cda1ea3caab3
Elena <110896958+Elena-web@users.noreply.github.com>
2024-04-02 15:01:58 +0300

    добавила таск robot-simulator

---
+++ robot_simulator/robot-simulator.js
@@ -0,0 +1,29 @@
+//
+// This is only a SKELETON file for the 'Robot Simulator' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class InvalidInputError extends Error {
+  constructor(message) {
+    super();
+    this.message = message || 'Invalid Input';
+  }
+}
+
+export class Robot {
+  get bearing() {
+    
+  }
+
+  get coordinates() {
+    
+  }
+
+  place({ x, y, direction }) {
+    
+  }
+
+  evaluate(instructions) {
+    
+  }
+}
