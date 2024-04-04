95f4cc4dd90aea8e8ba5c391486dfed024b237bd
Elena <110896958+Elena-web@users.noreply.github.com>
2024-04-04 09:35:18 +0300

    внесла изменения в условия

--- luhn/luhn.js
+++ luhn/luhn.js
@@ -5,19 +5,20 @@
 
 export const valid = (str) => {
   const arr = str.replace(/\s/g, '').split('');
-  if (arr.length > 1 && /^[0-9\s]+$/.test(str) && arr.length <= 20) {
+  if (arr.length > 1
+    && arr.every((element) => !Number.isNaN(parseInt(element, 10)))
+    && arr.length <= 20) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i += 1) {
-      if ((arr.length % 2 === 0 && (i === 0 || i % 2 === 0))
-      || (arr.length % 2 !== 0 && i % 2 !== 0)) {
-        if (arr[i] !== '9') {
-          arr[i] *= 2;
-          if (arr[i] > 9) {
-            arr[i] -= 9;
-          }
+    for (let i = arr.length - 2; i >= 0; i -= 2) {
+      if (arr[i] !== '9') {
+        arr[i] *= 2;
+        if (arr[i] > 9) {
+          arr[i] -= 9;
         }
       }
-      sum += Number(arr[i]);
+    }
+    for (let j = 0; j < arr.length; j += 1) {
+      sum += Number(arr[j]);
     }
     return sum % 10 === 0;
   }
